957885c560cb7fcb5da9333695a8f4d508ebc64e
Nastya <Nastya@DESKTOP-EU7BCQL>
2024-01-21 16:23:01 +0300

    Create Spring Project

---
+++ src/main/java/by/uviw/FormDocument/FormDocumentApplication.java
@@ -0,0 +1,13 @@
+package by.uviw.FormDocument;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FormDocumentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FormDocumentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/by/uviw/FormDocument/FormDocumentApplicationTests.java
@@ -0,0 +1,13 @@
+package by.uviw.FormDocument;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FormDocumentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
